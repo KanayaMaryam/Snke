d23c97e258c17672ea315e3047e6b0e8ba229e3b
zong <zongjohnzhang@gmail.com>
2016-02-13 16:12:38 -0800

    lol

--- app/src/main/java/com/fluttershy/snake/MainActivity.java
+++ app/src/main/java/com/fluttershy/snake/MainActivity.java
@@ -1,35 +1,45 @@
 package com.fluttershy.snake;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
     final int REQUEST_ENABLE_BT=69;
     ArrayAdapter<String> listAdapter;
     Button connectNew;
     ListView listView;
     BluetoothAdapter mBluetoothAdapter;
     Set<BluetoothDevice> devicesArray;
+    ArrayList<String> pairedDevices;
+    IntentFilter filter;
+    BroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3){
 
 
     }
@@ -48,14 +58,19 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             getPairedDevices();
+            startDiscovery();
         }
 
+    }
+    private void startDiscovery(){
+        mBluetoothAdapter.cancelDiscovery();
+        mBluetoothAdapter.startDiscovery();
     }
     public void getPairedDevices(){
         devicesArray=mBluetoothAdapter.getBondedDevices();
         if (devicesArray.size() > 0){
             for(BluetoothDevice device:devicesArray){
-                listAdapter.add(device.getName()+"\n"+device.getAddress());
+                pairedDevices.add(device.getName());
 
             }
         }
@@ -63,11 +78,56 @@ public class MainActivity extends AppCompatActivity {
     private void init(){
         connectNew=(Button)findViewById(R.id.btooth);
         listView=(ListView)findViewById(R.id.list);
+        listView.setOnItemClickListener(this);
         listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, 0);
         listView.setAdapter(listAdapter);
+        pairedDevices = new ArrayList<String>();
+        filter=new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        receiver =new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action=intent.getAction();
+                if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                    BluetoothDevice device=intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    String s="";
+                    for(int a=0; a<pairedDevices.size(); a++){
+                        if(device.getName().equals(pairedDevices.get(a))){
+                            s="(Paired)";
+                            break;
+                        }
+                    }
+                    listAdapter.add(device.getName()+"\n"+device.getAddress());
+                }
+                else if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)){
+
+                }
+                else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+
+                }
+                else if(BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)){
+                    if(mBluetoothAdapter.getState()==mBluetoothAdapter.STATE_OFF){
+                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+                    }
+
+                }
+            }
+        };
+        this.registerReceiver(receiver, filter);
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        this.registerReceiver(receiver, filter);
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(receiver, filter);
+        filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        this.registerReceiver(receiver, filter);
 
     }
-    protected void onActicityResult(int requestCode, int resultCode, Intent data){
+    protected void onPause(){
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode==RESULT_CANCELED){
             CharSequence text = "Y u no enable blutoth";
